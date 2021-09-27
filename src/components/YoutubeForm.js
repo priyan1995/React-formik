4e4c54ec37e2886e59abced03a31ce8b2308508d
priyan <45042030+priyan1995@users.noreply.github.com>
2021-09-27 22:54:35 +0530

    displaying errors

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -5,12 +5,12 @@ import { useFormik } from "formik";
 export const YoutubeForm = () => {
 
 
-    const initialValues = {       
-            name: '',
-            email: '',
-            channel: ''       
+    const initialValues = {
+        name: '',
+        email: '',
+        channel: ''
     }
-    
+
     const onSubmit = values => {
         console.log('Submitted', values)
     }
@@ -18,17 +18,17 @@ export const YoutubeForm = () => {
     const validate = values => {
         let errors = {}
 
-        if(!values.name){
+        if (!values.name) {
             errors.name = 'Required'
         }
 
-        if(!values.email){
+        if (!values.email) {
             errors.email = 'Required'
-        }else if(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(values.email)){
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
             errors.email = 'Invalid Email Format'
         }
 
-        if(!values.channel){
+        if (!values.channel) {
             errors.channel = 'Required'
         }
 
@@ -44,39 +44,50 @@ export const YoutubeForm = () => {
     })
 
     // console.log('Values: ', youtubeForm.values);
+   // console.log('Errors', youtubeForm.errors);
 
     return (
         <div>
 
             <form onSubmit={youtubeForm.handleSubmit}>
 
-                <label htmlFor="name">Name</label>
-                <input 
-                type="text" 
-                id="name" 
-                name="name" 
-                onChange={youtubeForm.handleChange}
-                value={youtubeForm.values.name}
-                />
+                <div className="form-control">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        onChange={youtubeForm.handleChange}
+                        value={youtubeForm.values.name}
+                    />
+                    {youtubeForm.errors.name ? <div className="error">{youtubeForm.errors.name}</div> : null}
+                </div>
 
-                <label htmlFor="email">Email</label>
-                <input 
-                type="email" 
-                id="email" 
-                name="email"
-                onChange ={youtubeForm.handleChange}
-                value={youtubeForm.values.email}
-                 />
 
-                <label htmlFor="channel">Channel</label>
-                <input 
-                type="text" 
-                id="channel" 
-                name="channel" 
-                onChange={youtubeForm.handleChange}
-                value={youtubeForm.values.channel}
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={youtubeForm.handleChange}
+                        value={youtubeForm.values.email}
+                    />
+                    {youtubeForm.errors.email ? <div className="error">{youtubeForm.errors.name}</div> : null}
+                </div>
 
-                />
+
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input
+                        type="text"
+                        id="channel"
+                        name="channel"
+                        onChange={youtubeForm.handleChange}
+                        value={youtubeForm.values.channel}
+                    />
+                    {youtubeForm.errors.channel ? <div className="error">{youtubeForm.errors.channel}</div> : null}
+                </div>
 
                 <button type="submit">Submit</button>
                 {/* to avoid warning please define button type as 'submit' */}
